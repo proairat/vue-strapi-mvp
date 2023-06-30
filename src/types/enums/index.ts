656ac25fa16a8ad4645ec55b98ef9da42b1d98e2
Shayakhmetov Airat Irekovich <proairat@gmail.com>
2023-06-30 13:44:16 +0500

    feature/AGIMA-1925-внедрил STRAPI. Задал тематику интернет-магазина продажи автомобилей. Отобразил данные на главной странице.

---
+++ src/types/enums/index.ts
@@ -0,0 +1,15 @@
+enum EHttpMethods {
+  GET="GET",
+  HEAD="HEAD",
+  POST="POST",
+  PUT="PUT",
+  DELETE="DELETE",
+  CONNECT="CONNECT",
+  OPTIONS="OPTIONS",
+  TRACE="TRACE",
+  PATCH="PATCH",
+}
+
+export {
+  EHttpMethods,
+};
